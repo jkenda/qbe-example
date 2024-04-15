bbd7bc79c5347fa9bbbec95b74140388e1d0f85a
Jakob Kenda <kenda.jakob@gmail.com>
2024-04-15 17:16:34 +0200

    reordered and optimized syscalls, cleaned up Makefile

--- Makefile
+++ Makefile
@@ -1,13 +1,12 @@
-CC_OPTS = -pipe -nostdlib -nodefaultlibs -xassembler -g0
+CC_OPTS=-c -pipe -nostdlib -nodefaultlibs -xassembler -g0
 
 all: example
 
 example: example.o syscalls.o
 	ld -o example example.o syscalls.o
-	strip example
 
 example.o: qbe/qbe example.ssa
-	cat example.ssa | qbe/qbe | cc -c -o example.o $(CC_OPTS) -
+	cat example.ssa | qbe/qbe | cc $(CC_OPTS) -o example.o -
 
 syscalls.o: syscalls.s
 	cc -c -o syscalls.o syscalls.s
@@ -15,8 +14,5 @@ syscalls.o: syscalls.s
 qbe/qbe: qbe/*.c qbe/*.h
 	cd qbe && make
 
-clean: qbe_clean
+clean:
 	rm -f example *.o
-
-qbe_clean:
-	cd qbe && make clean
--- syscalls.s
+++ syscalls.s
@@ -1,33 +1,26 @@
-.equ SYS_write, 1
-.equ SYS_exit , 60
+.equ WRITE, 1
+.equ EXIT , 60
 
 .text
-exit:
-    pushq %rbp
-    movq %rsp, %rbp
-
-    mov $SYS_exit, %rax
-
-    syscall
-    
-    leave
-    ret
-
-.globl write
-write:
-    push %rbp
-    mov %rsp, %rbp
-
-    mov $SYS_write, %rax
-
-    syscall
-
-    leave
-    ret
-
 .globl _start
 _start:
     # TODO: argc, argv
     call main
     mov %rax, %rdi
-    call exit
+    jmp exit
+
+.globl write
+write:
+    push %rcx
+    push %r11
+
+    mov $WRITE, %rax
+    syscall
+
+    pop %r11
+    pop %rcx
+    ret
+
+exit:
+    mov $EXIT, %rax
+    syscall
