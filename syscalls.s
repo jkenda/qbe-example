db079a536e55a4e0e82ace1bda7695927f301017
Jakob Kenda <kenda.jakob@gmail.com>
2024-04-18 21:25:08 +0200

    added argc, argv to _start, adapted qbe ssa to use argc

--- syscalls.s
+++ syscalls.s
@@ -1,26 +1,40 @@
-.equ WRITE, 1
-.equ EXIT , 60
-
 .text
+
 .globl _start
 _start:
-    # TODO: argc, argv
+    # get argc, argv
+    mov 0(%rsp), %rdi
+    lea 8(%rsp), %rsi
+
     call main
+
     mov %rax, %rdi
     jmp exit
 
-.globl write
-write:
+.macro wscall num
     push %rcx
     push %r11
 
-    mov $WRITE, %rax
+    mov \num, %rax
     syscall
 
     pop %r11
     pop %rcx
-    ret
 
-exit:
-    mov $EXIT, %rax
-    syscall
+    ret
+.endm
+
+
+.globl read
+.globl write
+.globl exit
+
+read:   wscall $READ
+write:  wscall $WRITE
+exit:   wscall $EXIT
+
+.equ READ , 0
+.equ WRITE, 1
+# ...
+.equ EXIT , 60
+# ...
