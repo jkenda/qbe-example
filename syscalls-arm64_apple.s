75e3a03715f821f11471d574ececba5af7909f25
Jakob Kenda <kenda.jakob@gmail.com>
2024-04-20 19:15:13 +0200

    fixed arm64_apple syscall numbers

--- syscalls-arm64_apple.s
+++ syscalls-arm64_apple.s
@@ -2,7 +2,7 @@
 .globl _start
 _start:
     ldr x0, [sp]
-    add x1, sp, #8
+    add x1, sp, 8
 
     bl _main
 
@@ -11,7 +11,7 @@ _start:
 
 .macro wscall num
     mov x16, \num
-    svc 0
+    svc 0x80
 
     ret
 .endm
@@ -20,13 +20,12 @@ _start:
 .globl _write
 .globl _exit
 
-_read:
-    wscall =READ
-_write:
-    wscall =WRITE
-_exit:
-    wscall =EXIT
+_read:  wscall =READ
+_write: wscall =WRITE
+_exit:  wscall =EXIT
 
-.equ READ,  0x2000003
-.equ WRITE, 0x2000004
-.equ EXIT,  0x2000001
+.equ EXIT,  1
+# ...
+.equ READ,  3
+.equ WRITE, 4
+# ...
